e26939ec8f1b78188f6c1fd61b5d0b948937efef
Sornsen007 <gustavoalmeidafernando65@gmail.com>
2025-04-30 01:38:15 +0000

    defini os operadores matemáticos para cálcular a quantidade de pessoas por km² Defini o printf para retornar o resultado

--- Xadrez.c
+++ Xadrez.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 
+
 int main() {
-
-
 
 printf("Provocando a existência do xadrez!\n");
 
@@ -13,7 +12,7 @@ printf("commint\n");
 
 char estado;
 
-char codigo[3];
+char codigo[100];
 
 char cidade[100];
 
@@ -23,7 +22,12 @@ float area;
 
 float pib;
 
-int turisticos[2];
+int turisticos;
+
+int soma, subtracao, divisao, multiplicacao;
+
+
+
 
 /* Criei os printf, para printar na tela as perguntas ao usuário.
 Também criei as scanf, para pedir ao usuário que responda as perguntas do printf criado e assim 
@@ -39,7 +43,7 @@ printf("Digite o nome da sua cidade: \n");
 scanf("%s", &cidade);
 
 printf("Qual o número da população da sua cidade: \n");
-scanf("%i", &populacao);
+scanf("%d", &populacao);
 
 printf("Qual a distância por km² da sua cidade: \n");
 scanf("%f", &area );
@@ -48,15 +52,21 @@ printf("Qual o PIB da sua cidade: \n");
 scanf("%f", &pib);
  
 printf("Qual a quantidade de pontos turistiscos que á em sua cidade: \n");
-scanf("%i", &turisticos);
+scanf("%d", &turisticos);
 
 
+divisao = populacao / area;
 /*Aqui criei as printf para retornar os dados que solicitei ao usuário, através das scanf. Utilizei
 também os especificadores para retornar os dados. */
 
-printf(" A estado selecionado é: %c \n \n O código da cidade é:  %s \n \n O nome da sua cidade é: %s \n \n A quantidade de habitantes é: %i \n", estado, codigo, cidade, populacao);
 
-printf("\n A distância por km² é: %2f \n \n O pib da sua cidade é: %f \n \n Os pontos turisticos da sua cidade é: %i ", area, pib, turisticos);
+
+
+
+printf(" \n A estado selecionado é: %c \n \n O código da cidade é:  %s \n \n O nome da sua cidade é: %s \n A quantidade de habitantes é: %d \n", estado, codigo, cidade, populacao);
+printf(" \n A distância por km² é: %.2f \n \n O pib da sua cidade é: %f \n \n Os pontos turisticos da sua cidade é: \n %d \n", area, pib, turisticos);
+
+printf("A quantidade de pessoas por km² é: %.2f \n", (float) divisao);
 
 
 return 0;
